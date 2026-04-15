6f8b31da0f51e896e14b17cadd6348142867d892
Joan Carlo <joaquinmeana7@gmail.com>
2026-04-15 17:57:58 -0300

    Agrege la navbar al header y la mayoria del carrucel

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,37 +1,46 @@
-<header class="navbar navbar-expand-lg bgCandy sticky-top">
-     <div class="container-fluid">
+<header class="navbar navbar-expand-lg bgCandy sticky-top d-block p-0">
+     <div class="container-fluid d-flex align-items-center py-2">
         <a href="/" class="btn btn-outline-light border-0 image-toggle-container">
             <!-- Imagen que se ve al inicio -->
-            <img src="/build/assets/img/logosinA.png" class="img-front" width="115px" height="auto">
+            <img src="/img/logosinA.png" class="img-front" width="115px" height="auto">
                         
             <!-- Imagen que se ve al tocar/hover -->
-            <img src="/build/assets/img/logosinB.png" class="img-back" width="115px" height="auto">
+            <img src="/img/logosinB.png" class="img-back" width="115px" height="auto">
         </a>
                 
         <div class="ms-auto d-flex align-items-center">
             <a href="/" class="btn btn-outline-light border-0 image-toggle-container">
                 <!-- Imagen que se ve al inicio -->
-                <img src="/build/assets/img/shopW.svg" class="img-front">
+                <img src="/img/shopW.svg" class="img-front">
                         
                 <!-- Imagen que se ve al tocar/hover -->
-                <img src="/build/assets/img/shopB.svg" class="img-back">
+                <img src="/img/shopB.svg" class="img-back">
             </a>
 
             <a href="/" class="btn btn-outline-light border-0 image-toggle-container">
                 <!-- Imagen que se ve al inicio -->
-                <img src="/build/assets/img/starW.svg" class="img-front">
+                <img src="/img/starW.svg" class="img-front">
                         
                 <!-- Imagen que se ve al tocar/hover -->
-                <img src="/build/assets/img/starB.svg" class="img-back">
+                <img src="/img/starB.svg" class="img-back">
             </a>
 
             <a href="/" class="btn btn-outline-light border-0 image-toggle-container">
                 <!-- Imagen que se ve al inicio -->
-                <img src="/build/assets/img/userW.svg" class="img-front">
+                <img src="/img/userW.svg" class="img-front">
                         
                 <!-- Imagen que se ve al tocar/hover -->
-                <img src="/build/assets/img/userB.svg" class="img-back">
+                <img src="/img/userB.svg" class="img-back">
             </a>
+        </div>
+    </div>
+
+    <div class="w-100 bg-dark text-white aling-items-center">
+        <div class="container d-flex justify-content-between align-items-center">
+            <a href="#" class="btn btn-outline-light border-0">Nosotros</a>
+            <a href="#" class="btn btn-outline-light border-0">Catalogos</a>
+            <a href="#" class="btn btn-outline-light border-0">Contactos</a>
+            <a href="#" class="btn btn-outline-light border-0">Legales</a>
         </div>
     </div>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,40 @@
     
 
     @section('contenido')
-        <div class="container-fluid">
-            <a href="/"> el qe lea es pvto</a>
+    <main class="grow-flex-1 mt-4 mb-4">
+        <div id="carruselPrincipal" class="carousel slide" data-bs-ride="carousel">
+        <!-- Indicadores (los puntitos de abajo) -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carruselPrincipal" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#carruselPrincipal" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#carruselPrincipal" data-bs-slide-to="2"></button>
+            </div>
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/img/CarrucelPC.png" class="d-none d-md-block w-100" alt="Banner Motos Desktop" style="max-height: 480px; object-fit: cover;; background-color: #00aff0;">
+                    <!-- IMAGEN PARA CELULAR: Se muestra solo en móviles -->
+                    <img src="/img/CarrucelM.png" class="d-block d-md-none w-100" style="height: 600px; object-fit: cover;" alt="Banner Motos Movil">
+                </div>
+                <div class="carousel-item">
+                    <img src="/img/Carrucel2PC.png" class="d-none d-md-block w-100" alt="Banner Eslogan Desktop" style="max-height: 480px; object-fit: cover;; background-color: #00aff0;">
+                    <img src="/img/Carrucel2M.png" class="d-block d-md-none w-100" style="height: 600px; object-fit: cover;" alt="Banner Eslogan Movil">
+                </div>
+                <div class="carousel-item">
+                    <img src="/img/Carrucel3PC.png" class="d-none d-md-block w-100" alt="Promoción 3" style="max-height: 480px; object-fit: cover;; background-color: #00aff0;">
+                    <img src="/img/Carrucel3M.png" class="d-block d-md-none w-100" style="height: 600px; object-fit: cover;" alt="Banner Motos Móvil">
+                </div>
+            </div>
+
+            <!-- Controles (flechas) -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#carruselPrincipal" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carruselPrincipal" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </button>
         </div>
+    </main>
     @endsection
     
     
